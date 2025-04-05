0fded19965413d81cfdcf814e40d234ea202ca8f
SonDQ(STLang) <sondoquang3@gmail.com>
2025-04-05 12:14:41 +0700

    Init Code add workflows github action

---
+++ src/test/java/com/stlang/githubactions/GithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stlang.githubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
