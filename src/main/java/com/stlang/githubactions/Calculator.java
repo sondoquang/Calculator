cc3778726a74ee9c48ae7af0826d1e350a9e8bc8
SonDQ(STLang) <sondoquang3@gmail.com>
2025-04-09 17:21:42 +0700

    Son Test Failed

--- src/main/java/com/stlang/githubactions/Calculator.java
+++ src/main/java/com/stlang/githubactions/Calculator.java
@@ -3,11 +3,11 @@ package com.stlang.githubactions;
 public class Calculator {
 
     public int add(int a, int b) {
-        return a + b;
+        return 5;
     }
 
     public int subtract(int a, int b) {
-        return a - b;
+        return 5;
     }
 
     public int multiply(int a, int b) {
